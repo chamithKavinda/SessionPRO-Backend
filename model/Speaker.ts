eeec2ba4b57962f16da59a9b2c4e72d9d8f7c12b
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2025-02-15 12:26:08 +0530

    feat: speaker model added

---
+++ model/Speaker.ts
@@ -0,0 +1,17 @@
+class Speaker {
+    id: string;
+    name: string;
+    bio: string;
+    expertise: string;
+    email: string;
+
+    constructor(id: string, name: string, bio: string, expertise: string, email: string) {
+        this.id = id;
+        this.name = name;
+        this.bio = bio;
+        this.expertise = expertise;
+        this.email = email;
+    }
+}
+
+export default Speaker;
